269c50e83658b9d9406a81890108d556d0f00b58
CaioRD <caiords67@gmail.com>
2024-06-10 19:02:17 -0300

    Create Solution.java

---
+++ Java-Arraylist/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s1 = sc.nextLine();  // Consume the newline character left by nextInt()
+        String s2 = sc.nextLine();
+        
+        System.out.println("Olá, " + s1 + " e "  + s2 + "!");
+        
+    }
+}
